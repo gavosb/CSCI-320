93ab406d3a2f8e65fe13694a70d0521679922bb3
Gavin <gavosb@protonmail.com>
2023-10-10 23:58:54 -0400

    working on random pt 2

--- xv6-riscv/user/useRandom.c
+++ xv6-riscv/user/useRandom.c
@@ -1,9 +1,12 @@
-#include "random.h"
 
-int main()
-{
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/random.h"
 
+int main() {
     rand_init(17);
-    for (int i = 0; i < 100; ++i)
+    for (int i = 0; i < 100; ++i) {
         printf("%d\n", scaled_random(1,1000));
+    }
+    exit(0);
 }
