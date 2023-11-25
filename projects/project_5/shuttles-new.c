c77a675771eb03d15bc81204187f26c4a32c68ff
Gavin <gavosb@protonmail.com>
2023-11-25 01:19:20 -0500

    more headway on shuttles

--- projects/project_5/shuttles-new.c
+++ projects/project_5/shuttles-new.c
@@ -7,6 +7,7 @@
 #include "zemaphore.h"
 
 Zem_t shuttle;
+Zem_t start_shuttle;
 Zem_t waiting_mutex_A;
 Zem_t waiting_mutex_B;
 Zem_t passengers_mutex;
@@ -25,26 +26,45 @@ int shuttle_capacity = 30;
  *
  */
 void *attendee(void *arg) {
-    printf("\n attendee initialized \n");
     
     while (1) {
         
-        /* wait at stop A */
+       /* wait at A */
         Zem_wait(&waiting_mutex_A);
         waiting_at_A++;
         Zem_post(&waiting_mutex_A);
 		Zem_wait(&bus_stop_A);
-        Zem_post(&shuttle);
+		
+        Zem_wait(&shuttle); // begin boarding
+        Zem_wait(&passengers_mutex);
+        Zem_wait(&waiting_mutex_A);
+        passengers++;
+        waiting_at_A--;
+        if (passengers == shuttle_capacity) { // if last passenger, tell shuttle to leave
+            Zem_post(&start_shuttle);
+        }
+        Zem_post(&passengers_mutex);
+        Zem_post(&waiting_mutex_A);
         Zem_wait(&arrived_B); // wait to arrive at B
         
-        /* wait at stop B */
-        
+        /* wait at B */
         Zem_wait(&waiting_mutex_B);
         waiting_at_B++;
         Zem_post(&waiting_mutex_B);
 		Zem_wait(&bus_stop_B);
-        Zem_post(&shuttle);
-        Zem_wait(&arrived_A); // wait to arrive at B
+
+        Zem_post(&shuttle); // begin boarding
+        Zem_wait(&waiting_mutex_A);
+        Zem_wait(&passengers_mutex);
+        passengers++;
+        waiting_at_A--;
+        if (passengers == shuttle_capacity) { // if last passenger, tell shuttle to leave
+            // technically this is unnecessary, but we dont want to post more than once to this
+            Zem_post(&start_shuttle);
+        }
+        Zem_post(&passengers_mutex);
+        Zem_post(&waiting_mutex_A);
+        Zem_wait(&arrived_A); // wait to arrive at A
         
     }
     
@@ -57,36 +77,23 @@ void *shuttle_driver(void *arg) {
     int capacity = (int) capacity_long;
     
     while (1) {
-        
-
-        // shuttle signals people to get on at bus stop (post to stop)
-        // people wait on bus as this happens
-        // as people get on, check if bus full & if anyone else waiting, if so, post to start_bus
-        // bus sleeps
-        // people wait on arrived_A
-        // people get off and wait on bus stop
-        // repeat steps for B on both shuttle and attendee
-        
 
         /* handle bus stop A */
         Zem_wait(&waiting_mutex_A);
         Zem_wait(&passengers_mutex);
         if (waiting_at_A != 0){ // pick up passengers at A
-            while (waiting_at_A != 0 && passengers < capacity) { // passengers < capacity || waiting_at_A == 0
-                Zem_post(&bus_stop_A);
-                waiting_at_A--;
-                passengers++;
+            while (waiting_at_A != 0 && passengers < capacity) {
+                Zem_post(&bus_stop_A); // wake up passengers
             }
-            
-        }//else{
-        //    Zem_post(&shuttle); // shuttle should be 0 here, make sure
-        //}
-        Zem_post(&shuttle); // set shuttle to 30+1?
-        
-        printf("\n leaving for A with %d passengers \n", passengers);
+        } else {
+            Zem_post(&start_shuttle); // just leave
+        }
+        printf("\n leaving for B with %d passengers \n", passengers);
         Zem_post(&passengers_mutex);
         Zem_post(&waiting_mutex_A);
-        Zem_wait(&shuttle); // leave
+        
+
+        Zem_wait(&start_shuttle);
         sleep(2); // travel to B
         Zem_wait(&waiting_mutex_B);
         Zem_wait(&passengers_mutex);
@@ -95,43 +102,46 @@ void *shuttle_driver(void *arg) {
         for (int i = 0; i < shuttle_capacity; i++) {
             Zem_post(&arrived_B); // wake up all passengers
         }
-        printf("\n arrived at A with %d passengers \n", passengers);
+        printf("\n arrived at B with %d passengers \n", passengers);
         Zem_post(&passengers_mutex);
         Zem_post(&waiting_mutex_B);
         /* finished with travel to B */
-        
-        /* handle bus stop B */
 
+        //
+        //
+        //
+
+        /* handle bus stop B */
         Zem_wait(&waiting_mutex_B);
         Zem_wait(&passengers_mutex);
         if (waiting_at_B != 0){ // pick up passengers at A
             while (waiting_at_B != 0 && passengers < capacity) {
-                Zem_post(&bus_stop_B);
-                waiting_at_B--;
-                passengers++;
+                Zem_post(&bus_stop_B); // wake up passengers
             }
-            
-        }else{
-            Zem_post(&shuttle); // shuttle should be 0 here, make sure
+        } else {
+            Zem_post(&start_shuttle); // just leave
         }
-        printf("\n leaving for B with %d passengers \n", passengers);
+        printf("\n leaving for A with %d passengers \n", passengers);
         Zem_post(&passengers_mutex);
         Zem_post(&waiting_mutex_B);
-        Zem_wait(&shuttle); // leave
+        
+
+        Zem_wait(&start_shuttle);
         sleep(2); // travel to B
         Zem_wait(&waiting_mutex_A);
         Zem_wait(&passengers_mutex);
-        waiting_at_A += passengers; // drop off passengers at A
+        waiting_at_A += passengers; // drop off passengers at B
         passengers = 0;
         for (int i = 0; i < shuttle_capacity; i++) {
             Zem_post(&arrived_A); // wake up all passengers
         }
-        printf("\n arrived at B with %d passengers \n", passengers);
+        printf("\n arrived at A with %d passengers \n", passengers);
         Zem_post(&passengers_mutex);
         Zem_post(&waiting_mutex_A);
-        
-    }    
+        /* finished with travel to B */
     
+    }
+
     return NULL;
 }
 
@@ -147,10 +157,11 @@ int main(int argc, char *argv[]) {
         When at 30, signals all waiting on the left/right condition to wake up.
         They then pile into the semaphore, then go to sleep again if not chosen.
     */
-    Zem_init(&shuttle, -shuttle_capacity);
+    Zem_init(&shuttle, shuttle_capacity);
     Zem_init(&waiting_mutex_A, 1);
     Zem_init(&waiting_mutex_B, 1);
     Zem_init(&passengers_mutex, 1);
+    Zem_init(&start_shuttle, 0);
     Zem_init(&bus_stop_A, 0);
     Zem_init(&bus_stop_B, 0);
     Zem_init(&arrived_A, 0);
